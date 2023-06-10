983f06d84d140c9978e9540d472d57b2838f0891
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-06-10 15:07:00 +0600

    print the longest non decreasing sub array

--- Longest_anything/longest_non_decreasing_subarray.cpp
+++ Longest_anything/longest_non_decreasing_subarray.cpp
@@ -1,27 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+void printLongestIncSubArr(int arr[], int n)
+{
+    int max = 1, len = 1, maxIndex = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+
+        if (arr[i] <= arr[i + 1])
+            len++;
+        else
+        {
+            if (max < len)
+            {
+                max = len;
+                maxIndex = i - max + 1;
+            }
+            len = 1;
+        }
+    }
+
+    if (max < len)
+    {
+        max = len;
+        maxIndex = n - max + 1;
+    }
+    for (int i = maxIndex; i < maxIndex + max; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 int main()
 {
     int n;
     cin >> n;
-    int arr[n + 3];
+    int arr[n + 4];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    int cnt = 1;
-    int mx = 1;
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (arr[i] <= arr[i + 1])
-        {
-            cnt++;
-        }
-        else
-        {
-            cnt = 1;
-        }
-       mx = max(cnt,mx);
-    }
-    cout << mx << endl;
+    printLongestIncSubArr(arr, n);
 }
